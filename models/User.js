96ad7f7dbcf596732f6a95d7645a739d795792cd
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-05-04 00:14:49 +0700

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const Task = require("./Task");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["manager", "employee"],
+      default: "employee",
+    },
+    // has a reference to the task model
+    // The ref option is what tells Mongoose which model to use during population
+    tasks: [{ type: Schema.Types.ObjectId, ref: Task }],
+    isDeleted: { type: Boolean, default: false, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
