23049d41151310c888c29eacacca1e437bbb7a52
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-05-04 00:14:47 +0700

    Create Task.js

---
+++ models/Task.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const { SchemaTypes } = mongoose;
+
+const taskSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: { type: String, required: true },
+    // pending: work not started, working: is working on it, review: waiting for review result, done: review is finished with satisfaction, archive: package as references for future
+    status: {
+      type: String,
+      enum: ["pending", "working", "review", "done", "archive"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+    // has a reference to the user model
+    user_name: { type: SchemaTypes.ObjectId, ref: "User" },
+    isDeleted: { type: Boolean, default: false, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Task = mongoose.model("Task", taskSchema);
+
+module.exports = Task;
