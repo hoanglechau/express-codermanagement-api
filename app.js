045ebd23ae00014649d965fd72b9889f1f323b3d
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-05-04 00:14:35 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,51 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require("dontenv").config();
+const express = require("express");
+const path = require("path");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Middlewares
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const notFound = require("./middlewares/notFound");
+const errorHandler = require("./middlewares/errorHandler");
 
-var app = express();
+// Routers
+const indexRouter = require("./routes/index");
 
-app.use(logger('dev'));
+// Database
+const connectDB = require("./db/connect");
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Routes
+app.use("/", indexRouter);
+
+// 404 error
+app.use(notFound);
+
+// Error handler
+app.use(errorHandler);
+
+// Connect to database and start server
+const port = process.env.PORT || 5000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
 
 module.exports = app;
